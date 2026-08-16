c62946d21a81f333fedcb531d369b784205179d9
Tienda digital <ea08031997@gmail.com>
2026-08-16 16:57:36 -0400

    chore: invalidate old catalog cache after studio update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='lovely-girl-v3';
+const CACHE='catalogoedit-v5';
 const CORE=['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./logo.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
