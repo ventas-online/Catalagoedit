05c2cb8ce4a08737bdccb6f6beb15e9b15acb4fa
Tienda digital <ea08031997@gmail.com>
2026-08-19 02:24:39 -0400

    refactor: separate product data

---
+++ data/products.js
@@ -0,0 +1,19 @@
+window.CATALOG_PRODUCTS = [
+  {
+    name: 'Producto de ejemplo 01',
+    brand: 'MARCA',
+    price: 0,
+    category: 'Categoría',
+    image: 'PRODUCTO_01.png',
+    featured: true,
+    description: 'Reemplaza este producto de ejemplo por el producto del cliente.'
+  },
+  {
+    name: 'Producto de ejemplo 02',
+    brand: 'MARCA',
+    price: 0,
+    category: 'Categoría',
+    image: 'PRODUCTO_02.png',
+    description: 'Reemplaza este producto de ejemplo por el producto del cliente.'
+  }
+];
