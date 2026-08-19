81a026509ee5708df3ac7647ddc16bd936c711b8
Tienda digital <ea08031997@gmail.com>
2026-08-19 02:24:34 -0400

    refactor: separate business configuration

---
+++ config/business.js
@@ -0,0 +1,6 @@
+window.CATALOG_CONFIG = {
+  businessName: 'NOMBRE DEL NEGOCIO',
+  whatsapp: 'NUMERO_WHATSAPP_CON_CODIGO_PAIS',
+  catalogUrl: 'https://TU-USUARIO.github.io/TU-REPOSITORIO/',
+  currency: 'USD'
+};
